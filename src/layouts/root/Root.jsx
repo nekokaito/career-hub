97984909ae4475a0d92ca77b1474fca5056f1156
nekokaito <siddiqsazzad001@gmail.com>
2024-03-25 11:26:04 +0600

    Created Links

--- src/layouts/root/Root.jsx
+++ src/layouts/root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/Footer";
+import NavBar from "../navbar/NavBar";
 
 const Root = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
